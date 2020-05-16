aebda6ad09b63965eba3b03041b8d2bda963b521
Megha K C <meghakc10@gmail.com>
2020-05-16 11:11:00 +0530

    Updated the program to refine the responses to React Web Tech

--- events.py
+++ events.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 from csv import writer
 
 url = 'https://www.meetup.com/find/?allMeetups=true&keywords=React&radius=25&userFreeform=New+York%2C+New+York%2C+USA&eventFilter=all'
@@ -8,7 +9,7 @@ data = response.text
 
 soup = BeautifulSoup(data, 'html.parser')
 events = soup.find_all('a', {'class':'display-none'})
-with open('events.csv', 'w') as csv_file:
+with open('event.csv', 'w') as csv_file:
 	csv_writer = writer(csv_file)
 	headers = ['Title', 'Link', 'Description', 'Date & Time', 'Cost', 'Location']
 	csv_writer.writerow(headers)
@@ -25,30 +26,32 @@ with open('events.csv', 'w') as csv_file:
 		if mem_int > 500:
 			# 1. Title of the event
 			title = event.text
-			print 'Title: ',title
+			# print 'Title: ',title
 
 			# 2. Link to the event		
-			print 'Link: ',link
+			# print 'Link: ',link
 
 			# 3. Description of the event
 			event_para = event_soup.find('p', {'class':'group-description'})
 			event_description = event_para.findNext('p').text
-			print 'Event description: ', event_description
+			if 'React' in event_description or 'React' in title:
+				# print 'Event description: ', event_description
 
-			# 4. Time and date of the event
-			time_exist = event_soup.find('time')
-			time = time_exist.text if time_exist else 'Not specified'
-			print 'Date & time: ',time
+				# 4. Time and date of the event
+				time_exist = event_soup.find('time')
+				time = time_exist.text if time_exist else 'Not specified'
+				# print 'Date & time: ',time
 
-			# 5. Cost of the event
-			cost_exist = event_soup.find(attrs={'data-e2e': 'event-footer--price-label'})
-			cost = cost.contents[0].text if cost_exist else 'FREE'
-			print 'Cost:', cost
-			# 6. Location of the event
-			location = event_soup.find('ul',{'class':'organizer-city'}).text
-			print 'Location: ',location
-			csv_writer.writerow([title.encode("utf-8"), link.encode("utf-8"), event_description.encode("utf-8"), time.encode("utf-8"), cost.encode("utf-8"), location.encode("utf-8")])
-			print '\n----------------------------------------------------'
+				# 5. Cost of the event
+				cost_exist = event_soup.find(attrs={'data-e2e': 'event-footer--price-label'})
+				cost = cost.contents[0].text if cost_exist else 'FREE'
+				# print 'Cost:', cost
+
+				# 6. Location of the event
+				location = event_soup.find('ul',{'class':'organizer-city'}).text
+				# print 'Location: ',location
+				csv_writer.writerow([title.encode("utf-8"), link.encode("utf-8"), event_description.encode("utf-8"), time.encode("utf-8"), cost.encode("utf-8"), location.encode("utf-8")])
+				# print '\n----------------------------------------------------'
 
 
 
